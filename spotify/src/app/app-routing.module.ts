e14a5c41651b83c6551e7967794abe213262d25a
Poliseno Riccardo <poliseno.riccardo@iisgalvanimi.edu.it>
2024-08-30 18:26:27 +0000

    finito

--- spotify/src/app/app-routing.module.ts
+++ spotify/src/app/app-routing.module.ts
@@ -3,12 +3,16 @@ import { Routes, RouterModule } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { SearchComponent } from './search/search.component';
 import { TrackComponent } from './track/track.component';
+import { ArtistComponent } from './artist/artist.component';
+import { AlbumComponent } from './album/album.component';
 
 const routes: Routes = [
   { path: '',   redirectTo: '/search', pathMatch: 'full' },
   { path: 'about', component: AboutComponent },
   { path: 'search', component: SearchComponent },
-  { path: 'tracks/:id', component: TrackComponent }
+  { path: 'tracks/:id', component: TrackComponent },
+  { path: 'artists/:id', component: ArtistComponent },
+  { path: 'albums/:id', component: AlbumComponent }
 ];
 
 @NgModule({
--- spotify/src/app/artist/artist.component.ts
+++ spotify/src/app/artist/artist.component.ts
@@ -1,5 +1,9 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router, ParamMap } from '@angular/router';
+import { SpotifyService } from '../spotify.service';
+import { Observable } from 'rxjs';
+import {Location} from '@angular/common'
+ 
 @Component({
   selector: 'app-artist',
   templateUrl: './artist.component.html',
@@ -7,4 +11,49 @@ import { Component } from '@angular/core';
 })
 export class ArtistComponent {
 
+  //Osserva gli eventi sulla route artists, restituisce la ParamMap che contiene tutti i   
+  //parametri passati all’url
+  routeObs !: Observable<ParamMap>; 
+  artistObs !: Observable<Object>;
+  albumObs !: Observable<Object>;
+  artist : any; //Qui salverò la traccia selezionata
+  artistsAlbums : any;
+  //Usiamo la dependency injection per farci mandare i moduli del routing e dello    
+  //SpotifyService
+  constructor(
+    private route: ActivatedRoute, 
+    private router: Router, 
+    private service: SpotifyService,
+    private location: Location
+    ) { }
+
+
+  ngOnInit(): void {
+    //Ottengo l'observable che notifica le informazioni sulla route attiva
+    this.routeObs = this.route.paramMap;
+    this.routeObs.subscribe(this.getRouterParam);
+  }
+
+  //Ogni volta che viene invocata la route artists/:id, l'observable richiama questo metodo
+  getRouterParam = (params: ParamMap) =>
+    {
+      let artistId = params.get('id') || ''; //Ottengo l'id dai parametri
+      console.log(artistId); //Stampo su console
+      //spotifyServiceObs va dichiarato
+      this.artistObs = this.service.getArtist(artistId);
+      this.artistObs.subscribe((data)=>{ this.artist = data; console.log(this.artist);})
+      
+      this.richiestaAlbums(artistId)
+    }
+
+    richiestaAlbums(artistId: string) {
+      this.albumObs = this.service.getArtistAlbums(artistId);
+      this.albumObs.subscribe((data)=>{ this.artistsAlbums = data; console.log(this.artistsAlbums);});
+      
+    }
+
+    back() : void
+    {
+      this.location.back();
+    }
 }
